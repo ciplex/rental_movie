0565ed5c5a440987ebb004fdd45dc5dd9cfb1862
ciplex <masterkemseng@yahoo.com>
2017-11-27 11:06:36 +0700

    add migration

--- database/migrations/2017_11_27_020840_create_movies_table.php
+++ database/migrations/2017_11_27_020840_create_movies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateMoviesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,18 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('movies', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role',['admin','guest']);
-            $table->rememberToken();
+            $table->integer('category_id');
+            $table->string('title');
+            $table->date('year');
+            $table->string('description');
+            $table->string('poster')->nullable();
             $table->timestamps();
+
+           
         });
+       
     }
 
     /**
@@ -31,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('movies');
     }
 }
