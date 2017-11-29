cf892539c1e16aa01d0cb53d2e1d02f7e43d0992
ciplex <masterkemseng@yahoo.com>
2017-11-29 17:38:54 +0700

    add index movie list

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -13,54 +13,41 @@
           </div>
         </div>
       </div>
-    </div>
   @endif
 
-<h1>Movie</h1>
-<form method="GET" action=" {{ route('movie.search') }}" class="form-inline my-2 my-lg-0 ml-5">
+<!-- <form method="GET" action=" {{ route('movie.search') }}" class="form-inline my-2 my-lg-0 ml-5">
 <input name="keyword" class="form-control" type="text" placeholder="searching..">
 </form>
 <form class="form-inline my-2 my-lg-0 ml-auto">
       <a href="{{route('movie.create')}}" class="btn btn-outline-primary my-2 my-sm-0">Tambah Data</a>
-    </form>
-  
-<table class="table table-hover table-striped table-dark">
-  <thead>
-    <tr>
-    <th scope="col">#</th>
-    <th scope="col">Poster</th>
-    <th scope="col">Category</th>
-    <th scope="col">Title</th>
-    <th scope="col">Year</th>
-    <th scope="col">Description</th>
-    <th scope="col">Action</th>
-    </tr>
-  </thead>
-  <tbody>
-  @foreach($movies as $movie)
-    <tr>
-        <td>{{ $movie->id }}</td>
-        <td>
-        @if($movie->photo)
-        <img src="{{ $movies->photo }}" width="100" height="100">
-        @else
-        <img src="/photos/no-avatar.png" width="100" height="100">
-        @endif
-        </td>
-        <td>{{ $movie->categories->name }}</td>
-        <td>{{ $movie->title }}</td>
-        <td>{{ $movie->year }}</td>
-        <td>{{ $movie->description }}</td>
-        <td>
-          <form method="POST" action="{{route('movie.destroy', $movie->id) }}" class="row no-gutters pb-2">
+    </form> -->
+   
+    <div class="row">
+        @foreach($movies as $movie)
+      <div class="col-12 col-md-3 py-2">
+        <div class="card">
+      @if($movie->photo)
+      <img src="{{ $movies->photo }}">
+      @else
+      <img src="/photos/no-avatar.png" width="100%" height="150px">
+      @endif
+      <div class="card-body">
+        <h4 class="card-title">{{ $movie->title }}</h4>
+        <p class="card-text">{{ $movie->categories->name }}</p>
+        <a href="#" class="btn btn-primary">Detail</a>
+      </div>
+      </div>
+     </div>
+      @endforeach
+      @stop
+    </div>
+    </div>
+          <!-- <form method="POST" action="{{route('movie.destroy', $movie->id) }}" class="row no-gutters pb-2">
             <input type="hidden" name="_method" value="delete" />
             {{ csrf_field() }}
             <button type="submit" onclick="return confirm('Hapus {{ $movie->category }} ?')" class="btn btn-danger col-sm-12">Delete</button>
           </form>    
             <a class="btn btn-primary col-sm-12" href="{{ route('movie.edit', $movie->id) }}">Update</a>
-        </tr>
-    @endforeach
-  </tbody>
-</table>
- {{ $movies->render() }}
-@stop
+        </tr> -->
+    
+ <!-- {{ $movies->render() }} -->
